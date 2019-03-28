42e76ddb551c9d362bb0126f4d5390de506df26e
Plinio Naves <pliniopjn@hotmail.com>
2019-03-28 18:30:29 -0300

    Gerando página de Login com Ionic CLI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './auth/pages/login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
