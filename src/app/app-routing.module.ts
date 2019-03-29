77352b116fd298f1b40a7236eeee52aeb34fb149
Plinio Naves <pliniopjn@hotmail.com>
2019-03-29 11:47:47 -0300

    Criando layout da página de Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './auth/pages/login/login.module#LoginPageModule' },
 ];
 
