ce33eeb53603a28cff8e11016c05a0daebab8b14
Plinio Naves <pliniopjn@hotmail.com>
2019-03-29 13:37:12 -0300

    Validando formulário com Reactive Forms

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  authForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
   }
 
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    console.log('AuthForm: ', this.authForm.value);
+  }
 }
