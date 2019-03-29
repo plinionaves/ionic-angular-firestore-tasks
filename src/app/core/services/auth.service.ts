8a7961cf9ff208a6e33262e0e985a19eec7b5338
Plinio Naves <pliniopjn@hotmail.com>
2019-03-29 18:05:49 -0300

    AuthService: login com Email e Password

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private afAuth: AngularFireAuth) {}
 
-  constructor() { }
+  private signInWithEmail({ email, password }): Promise<auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
 }
