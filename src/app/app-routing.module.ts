9f4187b84803f3e8451385a7777354d20c3e11eb
Plinio Naves <pliniopjn@hotmail.com>
2019-04-01 16:06:33 -0300

    Protegendo rotas com AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './core/guards/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './auth/auth.module#AuthModule' },
-  { path: 'tasks', loadChildren: './tasks/tasks.module#TasksModule' }
+  { path: 'tasks', loadChildren: './tasks/tasks.module#TasksModule', canLoad: [AuthGuard] }
 ];
 
 @NgModule({
--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '../core/guards/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
+    canActivateChild: [AuthGuard],
     children: [
       {
         path: '',
