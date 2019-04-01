51f6a26d46c7ed33db1770dfa22d2520ee21b0ef
Plinio Naves <pliniopjn@hotmail.com>
2019-04-01 18:21:48 -0300

    TaskItem para exibir uma única Tarefa

---
+++ src/app/tasks/components/task-item/task-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss'],
+})
+export class TaskItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
