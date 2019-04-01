eae6f18ee647006e24321fa53e32d2ed23480379
Plinio Naves <pliniopjn@hotmail.com>
2019-04-01 18:32:18 -0300

    TaskItem: Input e Output Properties

--- src/app/tasks/components/task-item/task-item.component.ts
+++ src/app/tasks/components/task-item/task-item.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss'],
+  styleUrls: ['./task-item.component.scss']
 })
-export class TaskItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class TaskItemComponent {
+  @Input() task: Task;
+  @Output() done = new EventEmitter<Task>();
+  @Output() update = new EventEmitter<Task>();
+  @Output() delete = new EventEmitter<Task>();
 }
