a5400518bb00c8272943ee2c63d89abd22720563
Plinio Naves <pliniopjn@hotmail.com>
2019-04-02 14:45:55 -0300

    TasksService: herdando Classe Abstrata Firestore

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { Firestore } from 'src/app/core/classes/firestore.class';
+import { Task } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TasksService {
-
-  constructor() { }
+export class TasksService extends Firestore<Task> {
+  constructor(db: AngularFirestore) {
+    super(db);
+  }
 }
