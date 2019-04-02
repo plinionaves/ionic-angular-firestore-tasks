c02beebc86b81672b30da0998820a5ab453ae79f
Plinio Naves <pliniopjn@hotmail.com>
2019-04-02 14:01:03 -0300

    Firestore: buscando registro por id

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -21,6 +21,10 @@ export abstract class Firestore<T extends { id: string }> {
     return this.collection.valueChanges();
   }
 
+  get(id: string): Observable<T> {
+    return this.collection.doc<T>(id).valueChanges();
+  }
+
   create(item: T): Promise<T> {
     item.id = this.db.createId();
     return this.setItem(item, 'set');
