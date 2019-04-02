acde7cf99c2dc5b1ee8405508555e6b4363f3e42
Plinio Naves <pliniopjn@hotmail.com>
2019-04-02 15:01:29 -0300

    Collection de Tarefas do usuário logado

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 
+import { AuthService } from 'src/app/core/services/auth.service';
 import { Firestore } from 'src/app/core/classes/firestore.class';
 import { Task } from '../models/task.model';
 
@@ -8,7 +9,18 @@ import { Task } from '../models/task.model';
   providedIn: 'root'
 })
 export class TasksService extends Firestore<Task> {
-  constructor(db: AngularFirestore) {
+  constructor(private authService: AuthService, db: AngularFirestore) {
     super(db);
+    this.init();
+  }
+
+  private init(): void {
+    this.authService.authState$.subscribe(user => {
+      if (user) {
+        this.setCollection(`/users/${user.uid}/tasks`);
+        return;
+      }
+      this.setCollection(null);
+    });
   }
 }
