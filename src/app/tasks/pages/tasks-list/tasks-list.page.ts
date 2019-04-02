ff86e37e48cf7b46a83bfdd129f69b0f9034f2c6
Plinio Naves <pliniopjn@hotmail.com>
2019-04-02 15:17:42 -0300

    Listando Tarefas do usuário na TasksListPage

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -2,23 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { Task } from '../../models/task.model';
+import { TasksService } from '../../services/tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.page.html',
-  styleUrls: ['./tasks-list.page.scss'],
+  styleUrls: ['./tasks-list.page.scss']
 })
 export class TasksListPage implements OnInit {
-
   tasks$: Observable<Task[]>;
 
-  constructor() { }
+  constructor(private tasksService: TasksService) {}
 
   ngOnInit(): void {
-    this.tasks$ = of([
-      { id: 'fd8g7df8g', title: 'Aprender Ionic', done: false },
-      { id: 'fd8g778df', title: 'Aprender Firestore', done: false }
-    ]);
+    this.tasks$ = this.tasksService.getAll();
   }
-
 }
