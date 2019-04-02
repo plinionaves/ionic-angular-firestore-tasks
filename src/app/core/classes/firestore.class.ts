2a7f0952af4db9465e679d27afb029fa573d1cdd
Plinio Naves <pliniopjn@hotmail.com>
2019-04-02 13:19:48 -0300

    Firestore Collections: listando registros

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -1,1 +1,16 @@
-export abstract class Firestore<T> {}
+import { AngularFirestore, AngularFirestoreCollection, QueryFn } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export abstract class Firestore<T> {
+  protected collection: AngularFirestoreCollection<T>;
+
+  constructor(protected db: AngularFirestore) {}
+
+  protected setCollection(path: string, queryFn?: QueryFn): void {
+    this.collection = path ? this.db.collection(path, queryFn) : null;
+  }
+
+  getAll(): Observable<T[]> {
+    return this.collection.valueChanges();
+  }
+}
