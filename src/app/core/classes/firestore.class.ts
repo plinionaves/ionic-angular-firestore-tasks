cc6365e5aed60dd051704f83adf57d9acc8b8c15
Plinio Naves <pliniopjn@hotmail.com>
2019-04-02 13:58:03 -0300

    Firestore: deletando registros existentes

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -29,4 +29,8 @@ export abstract class Firestore<T extends { id: string }> {
   update(item: T): Promise<T> {
     return this.setItem(item, 'update');
   }
+
+  delete(item: T): Promise<void> {
+    return this.collection.doc<T>(item.id).delete();
+  }
 }
