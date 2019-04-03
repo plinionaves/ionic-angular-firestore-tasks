2744471fb5527949e223822254dc9f1bfa323e0c
Plinio Naves <pliniopjn@hotmail.com>
2019-04-03 18:23:24 -0300

    Exibindo Loading ao enviar requisição ao Firestore

--- src/app/tasks/pages/task-save/task-save.page.ts
+++ src/app/tasks/pages/task-save/task-save.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 
+import { OverlayService } from 'src/app/core/services/overlay.service';
 import { TasksService } from '../../services/tasks.service';
 
 @Component({
@@ -15,6 +16,7 @@ export class TaskSavePage implements OnInit {
   constructor(
     private fb: FormBuilder,
     private navCtrl: NavController,
+    private overlayService: OverlayService,
     private tasksService: TasksService
   ) {}
 
@@ -30,12 +32,17 @@ export class TaskSavePage implements OnInit {
   }
 
   async onSubmit(): Promise<void> {
+    const loading = await this.overlayService.loading({
+      message: 'Saving...'
+    });
     try {
       const task = await this.tasksService.create(this.taskForm.value);
       console.log('Task created! ', task);
       this.navCtrl.navigateBack('/tasks');
     } catch (error) {
       console.log('Error saving Task: ', error);
+    } finally {
+      loading.dismiss();
     }
   }
 }
