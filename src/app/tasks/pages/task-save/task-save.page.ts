d053ab6147d1800446cb97340bd616049ccb5664
Plinio Naves <pliniopjn@hotmail.com>
2019-04-03 17:49:22 -0300

    Validando formulário com ReactiveForms

--- src/app/tasks/pages/task-save/task-save.page.ts
+++ src/app/tasks/pages/task-save/task-save.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-save',
   templateUrl: './task-save.page.html',
-  styleUrls: ['./task-save.page.scss'],
+  styleUrls: ['./task-save.page.scss']
 })
 export class TaskSavePage implements OnInit {
+  taskForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
   }
 
+  private createForm(): void {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      done: [false]
+    });
+  }
+
+  onSubmit(): void {
+    console.log('Task: ', this.taskForm.value);
+  }
 }
