7526ca0da129356b232eb82739931c306d18740c
Plinio Naves <pliniopjn@hotmail.com>
2019-04-03 18:49:24 -0300

    Navegando para rota de edição ao selecionar Tarefa

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { Observable, of } from 'rxjs';
 
 import { Task } from '../../models/task.model';
@@ -12,9 +13,13 @@ import { TasksService } from '../../services/tasks.service';
 export class TasksListPage implements OnInit {
   tasks$: Observable<Task[]>;
 
-  constructor(private tasksService: TasksService) {}
+  constructor(private navCtrl: NavController, private tasksService: TasksService) {}
 
   ngOnInit(): void {
     this.tasks$ = this.tasksService.getAll();
+  }
+
+  onUpdate(task: Task): void {
+    this.navCtrl.navigateForward(['tasks', 'edit', task.id]);
   }
 }
