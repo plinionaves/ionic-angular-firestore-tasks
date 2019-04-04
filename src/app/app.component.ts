7e7a92043205194eb2758384aaf4af04ea563efb
Plinio Naves <pliniopjn@hotmail.com>
2019-04-04 17:09:23 -0300

    Exibindo nome do usuário logado no Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,18 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AuthService } from './core/services/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
   pages: { url: string; direction: string; icon: string; text: string }[];
+  user: firebase.User;
 
   constructor(
+    private authService: AuthService,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -24,6 +28,8 @@ export class AppComponent {
       { url: '/tasks', direction: 'back', icon: 'checkmark', text: 'Tasks' },
       { url: '/tasks/create', direction: 'forward', icon: 'add', text: 'New Task' }
     ];
+
+    this.authService.authState$.subscribe(user => (this.user = user));
 
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
