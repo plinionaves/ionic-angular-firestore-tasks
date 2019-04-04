6db1653553a706795bff76454cac112525736689
Plinio Naves <pliniopjn@hotmail.com>
2019-04-04 18:22:50 -0300

    Habilitando Menu baseado no authState

--- src/app/shared/components/logout-button/logout-button.component.ts
+++ src/app/shared/components/logout-button/logout-button.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, Input, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
 
 import { AuthService } from 'src/app/core/services/auth.service';
 import { OverlayService } from 'src/app/core/services/overlay.service';
@@ -12,17 +12,23 @@ import { OverlayService } from 'src/app/core/services/overlay.service';
         <ion-icon name="exit" slot="icon-only"></ion-icon>
       </ion-button>
     </ion-buttons>
-  `,
+  `
 })
 export class LogoutButtonComponent implements OnInit {
+  @Input() menu: string;
 
   constructor(
     private authService: AuthService,
+    private menuCtrl: MenuController,
     private navCtrl: NavController,
     private overlayService: OverlayService
-  ) { }
+  ) {}
 
-  ngOnInit() {}
+  async ngOnInit(): Promise<void> {
+    if (!(await this.menuCtrl.isEnabled(this.menu))) {
+      this.menuCtrl.enable(true, this.menu);
+    }
+  }
 
   async logout(): Promise<void> {
     await this.overlayService.alert({
@@ -32,6 +38,7 @@ export class LogoutButtonComponent implements OnInit {
           text: 'Yes',
           handler: async () => {
             await this.authService.logout();
+            await this.menuCtrl.enable(false, this.menu);
             this.navCtrl.navigateRoot('/login');
           }
         },
@@ -39,5 +46,4 @@ export class LogoutButtonComponent implements OnInit {
       ]
     });
   }
-
 }
