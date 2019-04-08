1e61107c97853209cb8d75d7bb5669d2c460d1dd
Plinio Naves <pliniopjn@hotmail.com>
2019-04-08 17:38:14 -0300

    Configurações do Firebase para Produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD51NUSBXF5AcYmbpBhHADR7X0SVCZzhhg',
+    authDomain: 'ionic-firestore-tasks.firebaseapp.com',
+    databaseURL: 'https://ionic-firestore-tasks.firebaseio.com',
+    projectId: 'ionic-firestore-tasks',
+    storageBucket: 'ionic-firestore-tasks.appspot.com',
+    messagingSenderId: '1031886758700'
+  }
 };
