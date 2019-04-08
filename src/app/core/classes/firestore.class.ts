2ce922f27504cde2010ce387ae4e5a57e8a91492
Plinio Naves <pliniopjn@hotmail.com>
2019-04-08 12:51:58 -0300

    Modificando tipagem do método setItem

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -10,7 +10,7 @@ export abstract class Firestore<T extends { id: string }> {
     this.collection = path ? this.db.collection(path, queryFn) : null;
   }
 
-  private setItem(item: T, operation: 'set' | 'update'): Promise<T> {
+  private setItem(item: T, operation: string): Promise<T> {
     return this.collection
       .doc<T>(item.id)
       [operation](item)
